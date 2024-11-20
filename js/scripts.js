6713212baf4681b2a9108245a79b293278f99ae9
emmazgurski <emma.zgurski@uleth.ca>
2024-11-20 16:45:42 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+console.log("bonjour");
+
+console.log("oki")
+
+$("#num-19").append("<h2>World Toliet Day</h2>")
+$("#num-19").css("background-image", "url(imgs/wtd.webp)")
+$("#num-19").css("background-size", "cover")
+
+$("#num-20").addClass("wcd-bkg")
+$("#num-20").append("<h2>World Children's Day</h2>")
+
+$("#num-21").append("<h2>World Television Day</h2>")
+$("#num-21").css("background-image", "url(imgs/tv.jpeg)")
+$("#num-21").css("background-size", "cover")
+
+$("#num-25").append("<h2>International Day for the Elimination of Violence against Women</h2>")
+$("#num-25").css("background-image", "url(imgs/no-excuse.png)")
+$("#num-25").css("background-size", "cover")
+
+$("#num-26").append("<h2>World Sustainable Transport Day</h2>")
+$("#num-26").append("background-image", "url(imgs/bus.png)")
+$("#num-26").css("background-size", "cover")
+
